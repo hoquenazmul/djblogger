bbcc54f44cfbb08e12fcc836a116953b2bd4de48
Nazmul <technazmul.hoque@gmail.com>
2024-12-24 17:09:54 -0500

    Updated post model field

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title

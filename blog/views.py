236c880864755016eb770d26048adca2060d82e1
Nazmul <technazmul.hoque@gmail.com>
2024-12-25 01:14:02 -0500

    Loaded blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def hello(request):
-    return render(request, 'blog/post_list.html')
+    object_list = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'object_list': object_list})

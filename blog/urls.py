d22068ea52b70fa424a7e540e81801608033af62
Nazmul Hoque <technazmul.hoque@gmail.com>
2024-12-30 10:57:35 -0500

    Integreted post delete functionality

--- blog/urls.py
+++ blog/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('post/create/', views.PostCreateView.as_view(), name='post_create'),
-    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post_update'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]

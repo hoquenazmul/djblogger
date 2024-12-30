842b9e7c8d8755d842e3e2dccf29ce1f8587281a
Nazmul Hoque <technazmul.hoque@gmail.com>
2024-12-29 20:02:08 -0500

    Configured media settings for Dev and Updated profile model, template

--- djblogger/urls.py
+++ djblogger/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as users_views
 
@@ -12,3 +14,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 
 class Profile(models.Model):
-    image = models.ImageField(default='profile.jpg', upload_to='profile_pics')
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='profile_pics/profile.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
